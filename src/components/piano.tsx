2251d047d005e8152111463efd4e858775708e54
Cameron Edwards <edwards.cameron.pc@gmail.com>
2020-11-22 18:50:15 -0700

    improve the styling a bit

--- src/components/piano.tsx
+++ src/components/piano.tsx
@@ -3,8 +3,26 @@ import styled from '@emotion/styled'
 import parse from 'util/chord-parser'
 import Octave from './octave'
 
-const PianoWrapper = styled.div`
+const Wrapper = styled.div`
+  text-align: center;
+  padding: 1rem;
+`
+
+const Label = styled.label`
+  font-size: 2rem;
+`
+
+const Input = styled.input`
+  height: 42px;
+  padding: 8px;
+  vertical-align: middle;
+  font-size: 2rem;
+`
+
+const Keyboard = styled.div`
   display: flex;
+  justify-content: center;
+  margin-top: 2rem;
 `
 
 type Props = {}
@@ -13,16 +31,16 @@ const Piano: React.FC<Props> = ({}: Props) => {
   const [userInput, onChange] = useState('')
   const chord = parse(userInput)
   return (
-    <div>
-      <label>
-        chord:{' '}
-        <input value={userInput} onChange={(e) => onChange(e.target.value)} />
-      </label>
-      <PianoWrapper>
+    <Wrapper>
+      <Label>
+        {'Enter a chord: '}
+        <Input value={userInput} onChange={(e) => onChange(e.target.value)} />
+      </Label>
+      <Keyboard>
         <Octave base={0} notes={chord} />
         <Octave base={1} notes={chord} complete />
-      </PianoWrapper>
-    </div>
+      </Keyboard>
+    </Wrapper>
   )
 }
 
